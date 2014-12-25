e35c9c5294a627e638b3606f01d17bf32340459a
RussellLuo <luopeng.he@gmail.com>
2014-12-25 22:53:14 +0800

    bump version to 0.0.2

--- easyconfig/__init__.py
+++ easyconfig/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+__version__ = '0.0.2'
+
 from .config import Config
 from .objects import str_object, envvar_object
 from .mappings import json_mapping, yaml_mapping
